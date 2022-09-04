487cd3bc037680ef6eb2da70211c166eabf76b33
aecra <admin@aecra.cn>
2022-09-04 18:00:44 +0800

    fix: timezone support for cron

--- backend/corn/main.go
+++ backend/corn/main.go
@@ -1,12 +1,15 @@
 package corn
 
 import (
+	"time"
+
 	"github.com/aecra/covid/report"
 	"github.com/robfig/cron/v3"
 )
 
 func Start() {
-	c := cron.New()
+	nyc, _ := time.LoadLocation("Asia/Shanghai")
+	c := cron.New(cron.WithLocation(nyc))
 	if _, err := c.AddFunc("13 8,12 * * *", report.ReportAllClock); err != nil {
 		panic(err)
 	}
